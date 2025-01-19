b416a779a77947c1a695aabf46907bd8cd526473
Nam Truong <15520527@gm.uit.edu.vn>
2025-01-20 00:32:06 +0700

    set meta tag in app.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -33,10 +33,6 @@ export const Layout: FC<ILayoutProps> = ({ children, className, title }) => {
     <>
       <Head>
         <title>{title}</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
       </Head>
 
       <AnimatePresence>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import 'styles/override.css';
 import { ConfigProvider } from 'antd';
 import { Metadata } from 'next';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 export const metadata: Metadata = {
@@ -16,17 +17,47 @@ export const metadata: Metadata = {
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
-    <AntdRegistry>
-      <QueryClientProvider client={queryClient}>
-        <ConfigProvider
-          theme={{
-            components: {},
-          }}
-        >
-          <Component {...pageProps} />
-        </ConfigProvider>
-      </QueryClientProvider>
-    </AntdRegistry>
+    <>
+      <Head>
+        <title>Kronologic Paris 1920</title>
+        <meta name="title" content="Kronologic Paris 1920" />
+        <meta
+          name="description"
+          content="The app support checking result for Kronologic Paris 1920"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Kronologic Paris 1920" />
+        <meta
+          property="og:description"
+          content="The app support checking result for Kronologic Paris 1920"
+        />
+        <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Kronologic Paris 1920" />
+        <meta
+          property="twitter:description"
+          content="The app support checking result for Kronologic Paris 1920"
+        />
+        <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <AntdRegistry>
+        <QueryClientProvider client={queryClient}>
+          <ConfigProvider
+            theme={{
+              components: {},
+            }}
+          >
+            <Component {...pageProps} />
+          </ConfigProvider>
+        </QueryClientProvider>
+      </AntdRegistry>
+    </>
   );
 }
 
