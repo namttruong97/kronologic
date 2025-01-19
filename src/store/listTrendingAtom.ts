c82784a09391793e0595ebbc4352371a9c8a2d87
namtruongthanh <namttruong@kms-technology.com>
2025-01-19 23:09:24 +0700

    remove unse code

--- src/store/listTrendingAtom.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'jotai';
-
-import { TGifs } from 'model/gif.model';
-
-export const listTrendingAtom = atom<TGifs>([]);
-export const expandedImageAtom = atom<TGifs[number]>({} as TGifs[number]);
